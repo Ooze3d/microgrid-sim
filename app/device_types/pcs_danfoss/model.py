710ebfaa0132f1d18297d73be5fbeddab59c0339
Jose <Ooze3d@gmail.com>
2026-09-18 09:19:07 +0200

    refactor(pcs): changing PCS danfoss internal structure

--- app/device_types/pcs_danfoss/model.py
+++ app/device_types/pcs_danfoss/model.py
@@ -32,7 +32,7 @@ class PCSDanfossModel(BaseDeviceModel):
         )
 
         self.status_raw = int(
-            behaviour.get("status_raw", behaviour.get("status", 2))
+            behaviour.get("status_raw", behaviour.get("status", 1))
         )
 
         self.frequency_min = float(behaviour.get("frequency_min", 49.95))
